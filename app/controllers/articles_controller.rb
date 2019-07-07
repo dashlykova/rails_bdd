b6af5b32ab3701b9dede9895700f41eab1f05a2e
Daria Lykova <daria.lykova@gmail.com>
2019-07-07 21:47:22 +0200

    Adding back action to show.html.erb/Creating a notice for article successfully created/All tests passing now

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,8 +17,10 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+
     if @article.save
-      redirect_to @article
+      flash[:notice] = 'Article was successfully created.'
+      redirect_to article_path(@article)
     else
       render 'new'
     end
