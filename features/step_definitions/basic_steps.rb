88385742576f91734a3d06ae83207ba9af03bdc4
Daria Lykova <daria.lykova@gmail.com>
2019-07-15 11:30:11 +0200

    Creates a feature file for the 'user can login' function

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -1,4 +1,8 @@
-
+Given("the following user exists") do |table|
+    table.hashes.each do |user|        
+        FactoryBot.create(:user, user)
+    end
+end
 
 Given("the following articles exists") do |table|
     table.hashes.each do |article|
